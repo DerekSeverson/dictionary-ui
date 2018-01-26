a9845d6a986dff2bfea73a4914c7010a30c62969
Derek Severson <derek@swarmingtech.com>
2018-01-26 05:41:04 -0600

    Remove the unused search reducer.

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 
+// @NOTE: redux was not really used for this project,
+// but this is where each reducer would go. 
 
 export default combineReducers({
   router
